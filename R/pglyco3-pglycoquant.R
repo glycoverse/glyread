ee9b0550c8528af8a6296e0a49333fb2003da741
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-12-10 14:17:46 +0800

    feat: remove aggregation

--- R/pglyco3-pglycoquant.R
+++ R/pglyco3-pglycoquant.R
@@ -196,16 +196,7 @@ read_pglyco3_pglycoquant <- function(
       genes = stringr::str_remove(.data$genes, ";$")
     )
 
-  # ----- Aggregate quantification -----
-  # For label-free quantification, we sum the intensities of all
-  # spectra quantified for the same variable.
-  # A variable is defined as a unique combination of `names(new_names)`.
-  df <- df %>%
-    dplyr::summarize(
-      across(starts_with("Intensity"), sum),
-      .by = all_of(names(new_names))
-    )
-
+  # ----- Extract var_info -----
   var_info_cols <- names(new_names)
   var_info <- dplyr::select(df, all_of(var_info_cols))
 
