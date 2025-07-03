aa1f6d1aeaf17c52050a8db09e1efa273f518b5d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-07-03 15:40:14 +0800

    feat: add `read_byonic_pglycoquant()`

---
+++ R/byonic-pglycoquant.R
@@ -0,0 +1,160 @@
+#' Read Byonic-pGlycoQuant result
+#'
+#' If you used Byonic for intact glycopeptide identification,
+#' and used pGlycoQuant for quantification, this is the function for you.
+#' It reads in a pGlycoQuant result file and returns a [glyexp::experiment()] object.
+#' Currently only label-free quantification is supported.
+#'
+#' @section Which file to use?:
+#' You should use the "Quant.spectra.list" file in the pGlycoQuant result folder.
+#' Files from Byonic result folder are not needed.
+#' For instructions on how to use Byonic and pGlycoQuant, please refer to
+#' the manual: [pGlycoQuant](https://github.com/Power-Quant/pGlycoQuant/blob/main/Manual%20for%20pGlycoQuant_v202211.pdf).
+#' 
+#' @section Multisite glycopeptides:
+#' Currently, only single-site glycopeptides are supported.
+#' Multisite glycopeptides will be removed.
+#'
+#' @inheritSection read_pglyco3_pglycoquant Sample information
+#'
+#' @section Output:
+#' This function returns a [glyexp::experiment()] object.
+#'
+#' The following columns could be found in the variable information tibble:
+#' - `peptide`: character, peptide sequence
+#' - `glycan_composition`: [glyrepr::glycan_composition()], glycan compositions.
+#' - `peptide_site`: integer, site of glycosylation on peptide
+#' - `protein`: character, protein accessions
+#' - `protein_site`: integer, site of glycosylation on protein
+#'
+#' @param fp File path of the pGlycoQuant result file.
+#' @param sample_info File path of the sample information file (csv),
+#'  or a sample information data.frame/tibble.
+#' @param quant_method Quantification method. Either "label-free" or "TMT".
+#' @param glycan_type Glycan type. Either "N" or "O". Default is "N".
+#' @param sample_name_converter A function to convert sample names from file paths.
+#'  The function should take a character vector of old sample names
+#'  and return new sample names.
+#'  Note that sample names in `sample_info` should match the new names.
+#'  If NULL, original names are kept.
+#'
+#' @returns An [glyexp::experiment()] object.
+#' @seealso [glyexp::experiment()], [glyrepr::glycan_composition()]
+#'
+#' @importFrom magrittr %>%
+#' @importFrom tidyselect all_of
+#' @importFrom rlang .data
+#'
+#' @export
+read_byonic_pglycoquant <- function(
+  fp,
+  sample_info = NULL,
+  quant_method = c("label-free", "TMT"),
+  glycan_type = c("N", "O"),
+  sample_name_converter = NULL
+) {
+  # ----- Check arguments -----
+  checkmate::assert_file_exists(fp, access = "r", extension = ".list")
+  .check_sample_info_arg(sample_info)
+  quant_method <- rlang::arg_match(quant_method, c("label-free", "TMT"))
+  .check_sample_name_conv_arg(sample_name_converter)
+  glycan_type <- rlang::arg_match(glycan_type)
+
+  # ----- Read data -----
+  # Keep all PSM-level columns for variable info
+  if (quant_method == "label-free") {
+    exp <- .read_byonic_pglycoquant_label_free(
+      fp, sample_info, glycan_type, sample_name_converter
+    )
+  } else {
+    rlang::abort("TMT quantification is not supported yet.")
+  }
+
+  exp
+}
+
+
+.read_byonic_pglycoquant_label_free <- function(
+  fp,
+  sample_info,
+  glycan_type,
+  sample_name_converter
+) {
+  # ----- Read data -----
+  cli::cli_progress_step("Reading data")
+  df <- .read_byonic_file_into_tibble(fp)
+  df <- .remove_multisite_byonic(df)
+  expr_mat <- .extract_expr_mat_from_pglycoquant(df)
+  samples <- .extract_sample_names_from_pglycoquant(df, sample_name_converter)
+  sample_info <- .process_sample_info(sample_info, samples, glycan_type)
+  var_info <- .extract_var_info_from_byonic(df)
+  colnames(expr_mat) <- sample_info$sample
+  rownames(expr_mat) <- var_info$variable
+
+  # ----- Parse glycan compositions -----
+  cli::cli_progress_step("Parsing glycan compositions")
+  var_info <- dplyr::mutate(var_info,
+    glycan_composition = glyrepr::as_glycan_composition(.data$glycan_composition)
+  )
+
+  # ----- Pack Experiment -----
+  cli::cli_progress_step("Packing experiment")
+  glyexp::experiment(
+    expr_mat, sample_info, var_info,
+    exp_type = "glycoproteomics",
+    glycan_type = glycan_type,
+    quant_method = "label-free"
+  )
+}
+
+
+.read_byonic_file_into_tibble <- function(fp) {
+  col_types <- readr::cols(
+    Peptide = readr::col_character(),
+    `Protein Name` = readr::col_character(),
+    Position = readr::col_integer(),
+    Composition = readr::col_character(),
+  )
+  suppressWarnings(
+    suppressMessages(readr::read_tsv(fp, col_types = col_types, progress = FALSE)),
+    classes = "vroom_mismatched_column_name"
+  )
+}
+
+
+.remove_multisite_byonic <- function(df) {
+  new_df <- dplyr::filter(df, !stringr::str_detect(.data$Composition, stringr::fixed(",")))
+  n_removed <- nrow(df) - nrow(new_df)
+  perc_removed <- round(n_removed / nrow(df) * 100, 1)
+  cli::cli_alert_info("Removed {.val {n_removed}} ({.val {perc_removed}}%) multisite PSMs.")
+  new_df
+}
+
+
+.extract_var_info_from_byonic <- function(df) {
+  new_names <- c(
+    peptide = "Peptide",
+    protein = "Protein Name",
+    protein_site = "Position",
+    glycan_composition = "Composition"
+  )
+  df %>%
+    dplyr::select(all_of(new_names)) %>%
+    dplyr::mutate(
+      # K.N[+203.07937]GTR.G -> K.nGTR.G (mark glycosylated N)
+      peptide = stringr::str_replace(.data$peptide, "N\\[.+?\\]", "n"),
+      # K.nGTR.G -> nGTR (remove prefix and suffix)
+      peptide = stringr::str_sub(.data$peptide, 3L, -3L),
+      # Remove all other modifications like C[+57.02146] -> C
+      peptide = stringr::str_remove_all(.data$peptide, "\\[.+?\\]"),
+      # Extract peptide site (position of glycosylated residue)
+      peptide_site = stringr::str_locate(.data$peptide, "n")[, "start"],
+      # nGTR -> NGTR (restore N)
+      peptide = stringr::str_replace(.data$peptide, "n", "N"),
+      # >sp|P19652|A1AG2_HUMAN -> P19652
+      protein = stringr::str_extract(.data$protein, "(?:sp|tr)\\|(\\w+)\\|.*", group = 1),
+      # HexNAc(4)Hex(4)Fuc(1)NeuAc(1) -> HexNAc(4)Hex(4)dHex(1)NeuAc(1)
+      glycan_composition = stringr::str_replace(.data$glycan_composition, "Fuc", "dHex"),
+    ) %>%
+    dplyr::mutate(variable = stringr::str_c("PSM", dplyr::row_number()), .before = 1)
+}
---
+++ tests/testthat/test-byonic-pglycoquant.R
@@ -0,0 +1,245 @@
+# ----- Label-free basic functionality -----
+test_that("it returns correct information (label-free)", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+
+  expect_equal(
+    colnames(res$var_info),
+    c("variable", "peptide", "protein", "protein_site", "glycan_composition", "peptide_site")
+  )
+  expect_s3_class(res$var_info$glycan_composition, "glyrepr_composition")
+  expect_equal(colnames(res$sample_info), c("sample"))
+  expect_equal(colnames(res$expr_mat), c("20241224-lxj-nglyco-h_1"))
+  expect_equal(rownames(res$expr_mat), res$var_info$variable)
+  expect_equal(res$meta_data, list(
+    exp_type = "glycoproteomics",
+    glycan_type = "N",
+    quant_method = "label-free"
+  ))
+})
+
+# ----- Multisite glycopeptide filtering -----
+test_that("multisite glycopeptides are filtered in full workflow", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  # Check that no glycan compositions contain commas (indicating multisite)
+  compositions <- as.character(res$var_info$glycan_composition)
+  expect_false(any(stringr::str_detect(compositions, ",")))
+  
+  # Verify that the function processes the data and removes multisite entries
+  # This is inferred from the fact that all returned compositions are single-site
+  expect_true(nrow(res$var_info) > 0)
+  expect_true(all(nchar(compositions) > 0))
+})
+
+# ----- Peptide site calculation -----
+test_that("peptide_site and protein_site are correctly calculated", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  # Check that peptide_site column exists and contains valid integers
+  expect_true("peptide_site" %in% colnames(res$var_info))
+  expect_type(res$var_info$peptide_site, "integer")
+  expect_true(all(res$var_info$peptide_site > 0))
+  
+  # Check that protein_site column exists and contains integers
+  expect_true("protein_site" %in% colnames(res$var_info))
+  expect_type(res$var_info$protein_site, "integer")
+  expect_true(all(res$var_info$protein_site > 0))
+  
+  # Check that peptide sequences are properly processed (no modification marks)
+  peptides <- res$var_info$peptide
+  expect_true(all(nchar(peptides) > 0))
+  expect_false(any(stringr::str_detect(peptides, "\\[")))  # No modification brackets
+  expect_false(any(stringr::str_detect(peptides, "\\.")))  # No prefix/suffix dots
+})
+
+test_that("protein accessions are correctly extracted", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  # Check protein extraction from UniProt format
+  proteins <- res$var_info$protein
+  expect_true(all(nchar(proteins) > 0))
+  
+  # Check some specific examples we know should be in the data
+  expect_true(any(stringr::str_detect(proteins, "A0A024R6P0")))
+  expect_true(any(stringr::str_detect(proteins, "P19652")))
+})
+
+# ----- Glycan composition processing -----
+test_that("glycan compositions are correctly processed and parsed", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  # Check that all glycan compositions are valid glyrepr objects
+  expect_s3_class(res$var_info$glycan_composition, "glyrepr_composition")
+  expect_true(all(!is.na(res$var_info$glycan_composition)))
+  
+  # Check that Fuc has been converted to dHex in the compositions
+  # This is verified by checking that the compositions are properly parsed by glyrepr
+  # which expects dHex, not Fuc notation
+  compositions_str <- as.character(res$var_info$glycan_composition)
+  expect_true(all(nchar(compositions_str) > 0))
+  
+  # Verify that standard monosaccharide nomenclature is used
+  expect_false(any(stringr::str_detect(compositions_str, "Fuc")))  # Should be dHex
+  expect_true(any(stringr::str_detect(compositions_str, "dHex|HexNAc|Hex")))  # Contains standard notation
+})
+
+# ----- Variable naming -----
+test_that("variables are correctly named with PSM prefix", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  variables <- res$var_info$variable
+  expect_true(all(stringr::str_starts(variables, "PSM")))
+  expect_equal(length(variables), length(unique(variables)))  # All unique
+})
+
+# ----- Expression matrix processing -----
+test_that("intensity values are correctly processed", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  expr_mat <- res$expr_mat
+  
+  # Check that zero values are converted to NA
+  expect_true(all(is.na(expr_mat) | expr_mat > 0))
+  
+  # Check that the matrix is numeric
+  expect_true(is.numeric(expr_mat))
+  
+  # Check dimensions
+  expect_equal(nrow(expr_mat), nrow(res$var_info))
+  expect_equal(ncol(expr_mat), nrow(res$sample_info))
+})
+
+# ----- Sample name extraction -----
+test_that("sample names are correctly extracted from intensity columns", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  # Check that sample name matches the expected pattern
+  expect_equal(res$sample_info$sample, "20241224-lxj-nglyco-h_1")
+  expect_equal(colnames(res$expr_mat), "20241224-lxj-nglyco-h_1")
+})
+
+# ----- Error handling -----
+test_that("TMT quantification throws error", {
+  expect_error(
+    read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "TMT"
+    ),
+    "TMT quantification is not supported yet"
+  )
+})
+
+test_that("it handles O-linked glycan type", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free",
+      glycan_type = "O"
+    )
+  )
+  expect_equal(res$meta_data$glycan_type, "O")
+})
+
+# ----- Data consistency -----
+test_that("all output data types are consistent", {
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free"
+    )
+  )
+  
+  # Check data types of variable information
+  expect_type(res$var_info$peptide, "character")
+  expect_type(res$var_info$protein, "character")
+  expect_type(res$var_info$protein_site, "integer")
+  expect_type(res$var_info$peptide_site, "integer")
+  
+  # Check that all rows have data
+  expect_true(all(nchar(res$var_info$peptide) > 0))
+  expect_true(all(nchar(res$var_info$protein) > 0))
+  expect_true(all(!is.na(res$var_info$protein_site)))
+  expect_true(all(!is.na(res$var_info$peptide_site)))
+  expect_true(all(res$var_info$protein_site > 0))
+  expect_true(all(res$var_info$peptide_site > 0))
+})
+
+# ----- Sample name converter -----
+test_that("sample name converter works", {
+  sample_name_converter <- function(x) {
+    stringr::str_replace(x, "20241224-lxj-nglyco-h_1", "Sample_1")
+  }
+  
+  suppressMessages(
+    res <- read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free",
+      sample_name_converter = sample_name_converter
+    )
+  )
+  
+  expect_equal(colnames(res$expr_mat), "Sample_1")
+  expect_equal(res$sample_info$sample, "Sample_1")
+})
+
+# ----- Parameter validation (minimal, since common validation is tested in pglyco3) -----
+test_that("it validates quant_method parameter", {
+  expect_error(
+    read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "invalid_method"
+    ),
+    "must be one of"
+  )
+})
+
+test_that("it validates glycan_type parameter", {
+  expect_error(
+    read_byonic_pglycoquant(
+      test_path("byonic-pglycoquant-LFQ-result.list"),
+      quant_method = "label-free",
+      glycan_type = "invalid_type"
+    ),
+    "must be one of"
+  )
+})
